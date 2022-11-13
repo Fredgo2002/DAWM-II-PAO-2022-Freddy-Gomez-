b02cee69abb9e64a24267a739e631bf2567c3cef
Fredgo2002 <fagomez@espol.edu.ec>
2022-11-13 16:06:53 -0500

    script dashboard

--- proyecto02/public/scripts/scripts.js
+++ proyecto02/public/scripts/scripts.js
@@ -1,5 +1,30 @@
-function bienvenida(){
-    alert("Bienvenido al proyecto 02")
-}
-
-bienvenida();
+let cargarDatos = () => {
+    fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY")
+      .then(response => response.text())
+      .then(data => {
+      data= JSON.parse(data)
+      console.log(data)
+      //photos = data.photos
+      for (photo of data["photos"]){
+        let id= photo.id
+        let sol= photo.sol
+        let img = photo.img_src
+        let date = photo.earth_date
+        let plantilla = 
+        `<tr>
+          <td>${id}</td>
+          <td>${sol}</td>
+          <td>${img}</td>
+          <td>${date}</td>
+         </tr>`
+        document.querySelector(".containerTabla .tabla-datos .datos").innerHTML += plantilla
+    }  
+      })
+      
+      .catch(console.error);
+  
+  }
+ 
+window.addEventListener('DOMContentLoaded', (event) => {
+   cargarDatos()
+});
